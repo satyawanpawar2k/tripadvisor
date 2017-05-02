b2e6ce5d94cfe4656d69c9b7c0aca25ed30d25c5
satyawanpawar2k <satyawan.pawar@infostretch.com>
2017-05-02 13:16:33 +0530

    Initial Commit 3

--- Tripadvisor/src/test/java/com/tripadvisor/tests/FindFlights.java
+++ Tripadvisor/src/test/java/com/tripadvisor/tests/FindFlights.java
@@ -3,10 +3,19 @@
  */
 package com.tripadvisor.tests;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
 /**
  * @author satyawan.pawar
  *
  */
 public class FindFlights {
 
+	WebDriver driver;
+	utilsMethods UM = new utilsMethods();
+	@Test
+	public void findFlights(){
+		UM.initBrowser(driver, "chrome");
+	}
 }
